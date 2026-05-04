6ff1cdf4994f6b3369104d68e5c847b5bfe9a4a3
Kyle Engler <engler730@gmail.com>
2026-05-04 09:54:58 -0400

    Update demo mode

--- src/main/java/com/kyleengler/commonwealthsavers/AccountManager.java
+++ src/main/java/com/kyleengler/commonwealthsavers/AccountManager.java
@@ -4,45 +4,55 @@ public class AccountManager {
 
     public static void main(String[] args) {
         System.out.println("Running in demo mode.");
-        runDemo();
+        try {
+            runDemo();
+        } catch (RuntimeException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     private static void runDemo() {
-        // Demo transfer funds evenly
-        Account source = new Account("source", 10000.0);
-        Account target1 = new Account("target1", 0.0);
-        Account target2 = new Account("target2", 0.0);
-        Account target3 = new Account("target3", 0.0);
-
-        System.out.println("Accounts initialized.");
-        printAccounts(source, target1, target2, target3);
-
-        System.out.println("Transferring funds evenly between target accounts.");
-        System.out.println("Initializing transfer...");
-        source.transferEntireBalanceEvenly(target1, target2, target3);
-
-        System.out.println("Transfer completed.");
-        printAccounts(source, target1, target2, target3);
-
-        // Demo distribute funds
-        System.out.println("\n================================\n");
-        System.out.println("Accounts reinitialized.");
-        double transferAmount = 100.10;
-        source = new Account("source", 10000.0);
-        target1 = new Account("target1", 0.0);
-        target2 = new Account("target2", 0.0);
-        target3 = new Account("target3", 0.0);
-        printAccounts(source, target1, target2, target3);
-
-        System.out.printf("Transferring $%.2f from source to each target.\n", transferAmount);
-        System.out.println("Initializing transfer...");
-        source.transfer(transferAmount, target1, target2, target3);
-
-        System.out.println("Transfer completed.");
-        printAccounts(source, target1, target2, target3);
+        transferEntireBalanceEvenly(10000.0, 3);
+        System.out.println("\n======================================\n");
+        transferBalance(10000, 3, 100.10);
     }
 
-    public static void printAccounts(Account... accounts) {
+    private static void transferBalance(double sourceBalance, int targetAccountsCount, double transferAmount) {
+        Account source = new Account("source", sourceBalance);
+        Account[] targets = new Account[targetAccountsCount];
+        for (int i = 0; i < targetAccountsCount; i++) {
+            targets[i] = new Account("target" + i, 0.0);
+        }
+        System.out.println("Accounts initialized.");
+        printAccounts(source);
+        printAccounts(targets);
+        System.out.printf("Transferring $%.2f from source to each target.\n", transferAmount);
+        System.out.println("Initializing transfer...");
+        source.transfer(transferAmount, targets);
+        System.out.println("Transfer completed.");
+        printAccounts(source);
+        printAccounts(targets);
+    }
+
+    private static void transferEntireBalanceEvenly(double sourceBalance, int targetAccountsCount) {
+        Account source = new Account("source", sourceBalance);
+        Account[] targets = new Account[targetAccountsCount];
+        for (int i = 0; i < targetAccountsCount; i++) {
+            targets[i] = new Account("target" + i, 0.0);
+        }
+        System.out.println("Accounts initialized.");
+        printAccounts(source);
+        printAccounts(targets);
+        System.out.println("Transferring funds evenly between target accounts.");
+        System.out.println("Initializing transfer...");
+        source.transferEntireBalanceEvenly(targets);
+        System.out.println("Transfer completed.");
+        printAccounts(source);
+        printAccounts(targets);
+    }
+
+    private static void printAccounts(Account... accounts) {
         for (Account account : accounts) {
             System.out.println(account);
         }
